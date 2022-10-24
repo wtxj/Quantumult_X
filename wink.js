e746fe8199130610a4e6f534eeb1a426981de1d1
wtxj <116582564+wtxj@users.noreply.github.com>
2022-10-25 00:20:51 +0800

    Update wink.js

--- wink.js
+++ wink.js
@@ -1,7 +1,23 @@
-echo "# Quantumult_X" >> README.md
-git init
-git添加README.md
-git commit -m "first commit"
-git分支-M主
-git远程添加源 https://github.com/wtxj/Quantumult_X.git
-git push -u origin main
+
+/******************************
+
+[rewrite_local]
+^https:\/\/api-sub\.meitu\.com url script-response-body Wink.js
+[mitm] 
+hostname = api-sub.meitu.com
+
+********************************/
+
+
+var body = $response.body;
+var url = $request.url;
+var obj = JSON.parse(body);
+
+const vip = '/user';
+
+
+if (url.indexOf(vip) != -1) {
+    obj.data.is_vip = true;
+	body = JSON.stringify(obj);
+}
+$done({body});
