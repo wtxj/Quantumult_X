6b462112c888dc31d48b49cfc74ab44be099f55d
wtxj <116582564+wtxj@users.noreply.github.com>
2022-10-27 17:09:08 +0800

    Update aptv.js

--- aptv.js
+++ aptv.js
@@ -1,6 +1,6 @@
 /******************************
 [rewrite_local]
-^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body aptv.js
+^https:\/\/buy\.itunes\.apple\.com\/verifyReceipt url script-response-body https://raw.githubusercontent.com/wtxj/Quantumult_X/main/aptv.js
 [mitm] 
 hostname = buy.itunes.apple.com
 *******************************/
