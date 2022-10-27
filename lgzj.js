fb13f2d44d3950b2fef7ebee2d9f65d14bb63e71
wtxj <116582564+wtxj@users.noreply.github.com>
2022-10-27 17:18:46 +0800

    Create lgzj.js

---
+++ lgzj.js
@@ -0,0 +1,15 @@
+/*******************************
+脚本：灵感足迹
+[rewrite_local]
+^https:\/\/footprint-api\.quthing\.com\/vip\/state url script-response-body lgzj.js
+[mitm] 
+hostname = footprint-api.quthing.com
+
+*******************************/
+var body=$response.body;
+body = body.replace(/"validVip\":\w+/g,'"validVip":true');
+body = body.replace(/"expireTime\":\d+/g,'"expireTime":9999999999999');
+body = body.replace(/"trialPeriod\":\w+/g,'"trialPeriod":true');
+body = body.replace(/"checkIsExpire\":false/g,'"checkIsExpire":true');
+body = body.replace(/"checkIsVip\":false/g,'"checkIsVip":true');
+$done(body);
